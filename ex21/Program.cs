1b8757f128cbeaf9f2af62a987386336c00d42a5
Delyash <delyash91@mail.ru>
2023-02-09 21:50:44 -0500

    ex211

--- ex21/Program.cs
+++ ex21/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿double Distance (int xa, int ya, int za, int xb, int yb, int zb)
+{
+    int deltaX = xa - xb;
+    int deltaY = ya - yb;
+    int deltaZ = za - zb;
+    double distAB = Math.Round(Math.Sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ),2);
+    return distAB;
+}
+Console.WriteLine("input x coordinat of a ");
+int xAcoord = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y coordinat of a ");
+int yAcoord = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z coordinat of a ");
+int zAcoord = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input x coordinat of B ");
+int xBcoord = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y coordinat of b ");
+int yBcoord = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z coordinat of b ");
+int zBcoord = Convert.ToInt32(Console.ReadLine());
+
+double distanceAB = Distance (xAcoord, yAcoord, zAcoord, xBcoord, yBcoord, zBcoord);
+Console.WriteLine($"distance between a and b is {distanceAB}");
