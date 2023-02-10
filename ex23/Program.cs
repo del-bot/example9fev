b70271705ff4f9ae4209b7409b73e372fe800249
Delyash <delyash91@mail.ru>
2023-02-09 21:52:45 -0500

    ex232

--- ex23/Program.cs
+++ ex23/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Quards(int uN)
+{
+    int current = 1;
+    while (current <= uN)
+    {
+        double res = Math.Pow(current, 3);
+        Console.WriteLine($"{res}, ");
+        current++;
+    }
+    Console.WriteLine("\b\b.");
+}
+Console.Write("input number");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if(user_number >= 1)
+{
+    Console.Write($"{user_number} -> ");
+    Quards(user_number);
+}
+else Console.WriteLine("impossible value");
