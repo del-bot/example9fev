a30a8cba56cbe9bc10b3f04a9c79c1bd3f5cd6f9
Delyash <delyash91@mail.ru>
2023-02-09 21:48:36 -0500

    ex199

--- ex19/Program.cs
+++ ex19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Polindr(string number)
+{
+    int j = number.Length-1;
+    for ( int i = 0; i < j-i; i++)
+    {
+        if (number[i] == number[j-i])
+        Console.WriteLine("yes, it is polyndrom");
+        else Console.WriteLine("no, it is not");
+        i++;
+    }
+}
+Console.Write("input number");
+string? UserNumber = Console.ReadLine();
+if (UserNumber!.Length == 5)
+{
+    Polindr(UserNumber);
+}
+else Console.WriteLine("input correct number");
